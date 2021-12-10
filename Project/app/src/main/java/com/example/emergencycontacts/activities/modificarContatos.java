101f1194e73a35946133e0584aa5b72eef19038b
Samuel Afonso Gomes <samuel.gomes.afonso@gmail.com>
2021-12-10 20:39:14 -0300

    Bottom Menu
    Add bottom menu icons
    Add Bottom menu activity
    Add bottom menu in other activities
    Add some extra stuff i don't remember

--- Project/app/src/main/java/com/example/emergencycontacts/activities/modificarContatos.java
+++ Project/app/src/main/java/com/example/emergencycontacts/activities/modificarContatos.java
@@ -4,17 +4,25 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.ListView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.emergencycontacts.R;
 import com.example.emergencycontacts.objects.Contato;
 import com.example.emergencycontacts.objects.User;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class modificarContatos extends AppCompatActivity {
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectOutputStream;
+import java.nio.charset.StandardCharsets;
+
+public class modificarContatos extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
     boolean fTuser = true;
     EditText edtPesquisa;
     ListView listaContCelular;
@@ -46,8 +54,35 @@ public class modificarContatos extends AppCompatActivity {
         int num = userContato.getInt("numContatos", 0);
         SharedPreferences.Editor editorContato = userContato.edit();
 
+        try {
+            ByteArrayOutputStream dt = new ByteArrayOutputStream();
+            ObjectOutputStream oos = new ObjectOutputStream(dt);
+            dt = new ByteArrayOutputStream();
+            oos = new ObjectOutputStream(dt);
+            oos.writeObject(c);
+            String contatoSerializado= dt.toString(StandardCharsets.ISO_8859_1.name());
+            editorContato.putString("contato"+(num+1), contatoSerializado);
+            editorContato.putInt("numContatos",num+1);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.v("EmContacts", "Headshoot ~ Activity Lista de Contatos");
+    }
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.v("EmContacts", "Kill Confirmed ~ Activity Lista de Contatos");
+    }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        //add depois de ter add o menu
+        return false;
     }
 }
 
